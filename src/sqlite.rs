da932a9c9e5efaf2b78a398327dba81da9fb10b8
acalanato <vagner.assistencia@gmail.com>
2024-06-29 19:54:53 -0300

    Write to file

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,29 +1,37 @@
+const BASE: &str = "users.db";
+
 pub struct Item {
     pub name: String,
     pub stock: u32,
 }
 
 pub mod sqlite {
-    use super::Item;
+    use super::{Item, BASE};
     pub fn create(item: Item) {
-	let connection = sqlite::open(":memory:").unwrap();
+	let connection = sqlite::open(BASE).unwrap();
 	let query = format!("
 CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER);
 INSERT INTO users VALUES ('{0}', {1});
 ",item.name, item.stock);
 	connection.execute(query).unwrap()
     }
-    pub fn read() {
-	let connection = sqlite::open(":memory:").unwrap();
-
-	let query = "
-CREATE TABLE users (name TEXT, age INTEGER);
-INSERT INTO users VALUES ('Alice', 42);
-";
-	connection.execute(query).unwrap()
+    pub fn read(q: String) {
+	let connection = sqlite::open(BASE).unwrap();
+	let query = format!("
+SELECT * FROM {};
+", q);
+	connection
+            .iterate(query, |pairs| {
+                for &(name, value) in pairs.iter() {
+                    println!("{} - {}", name, value.unwrap());
+                }
+                true
+            })
+            .unwrap();
     }
+    
     pub fn update() {
-	let connection = sqlite::open(":memory:").unwrap();
+	let connection = sqlite::open(BASE).unwrap();
 
 	let query = "
 CREATE TABLE users (name TEXT, age INTEGER);
@@ -31,8 +39,9 @@ INSERT INTO users VALUES ('Alice', 42);
 ";
 	connection.execute(query).unwrap()
     }
+    
     pub fn delete() {
-	let connection = sqlite::open(":memory:").unwrap();
+	let connection = sqlite::open(BASE).unwrap();
 
 	let query = "
 CREATE TABLE users (name TEXT, age INTEGER);
