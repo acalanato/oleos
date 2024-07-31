5810fcfc266bb0e096c4656f40667271e3d86519
acalanato <vagner.assistencia@gmail.com>
2024-07-30 21:01:23 -0300

    Partially fixed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-use crate::sqlite::{sqlite::{create, list_all}, Item};
-=======
-use crate::sqlite::{sqlite::{create, read}, Item};
->>>>>>> main
+use crate::sqlite::{sqlite::{create, list_all, read}, Item};
 
 pub mod sqlite;
 
 fn main() {
-<<<<<<< HEAD
     let melaleuca = Item {
 	name: String::from("Alecrim"),
 	stock: 3,
@@ -15,12 +10,10 @@ fn main() {
     create(melaleuca);
     list_all();
     println!("Sucess!");
-=======
     let item = Item {
 	name: String::from("Melaleuca"),
 	stock: 1,
     };
     create(item);
-    read(String::from("1"));
->>>>>>> main
+    read();
 }
